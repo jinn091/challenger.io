902cef85bd1f1a29641c8d8ee62c12d9d7b1952a
jinn091 <dev.jinn221@gmail.com>
2024-10-12 15:42:16 +0630

    fix: Managing packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { flatRoutes } from "remix-flat-routes"; // Import flatRoutes
+import { flatRoutes } from "remix-flat-routes";
 
 export default defineConfig({
   plugins: [
